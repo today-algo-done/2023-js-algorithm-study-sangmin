c409e5d51bf6e21542e6edb0167a47e41cec27c0
Citizen <40601526+ViGilanteAF@users.noreply.github.com>
2022-12-30 11:51:59 +0900

    Solve Search

---
+++ Week2/Linear_Search_Exercise.js
@@ -0,0 +1,9 @@
+function linearSearch(a, b) {
+    for (let i = 0; i < a.length; ++i) {
+        if (a[i] === b) {
+            return i
+        }
+
+    }
+    return -1;
+}
--- Week2/fib.js
+++ Week2/fib.js
@@ -2,7 +2,13 @@
 // fib(10) // 55
 // fib(28) // 317811
 // fib(35) // 9227465
-/** 피보나치 수열 알고리즘 */
+/** 피보나치 수열 알고리즘 
+ * recursiveRange 가 힌트가 뒬수있음 
+ * 주어진 숫자 에서 -1 , -2 를 해준뒤 서로를 더하게끔 하면 됨
+ * 입력을 받은 숫자는 덧셈을 하는 횟수 임으로 
+ * 최대 덧셈횟수값 이라는것을 생각하면 됨
+*/
+
 
 function fib(a) {
     // add whatever parameters you deem necessary - good luck!  
