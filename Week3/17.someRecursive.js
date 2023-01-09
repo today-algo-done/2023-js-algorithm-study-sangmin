a6c8ec9292bc2477d0bbc8072273dcbf3cad3e99
Citizen <40601526+ViGilanteAF@users.noreply.github.com>
2023-01-09 23:21:30 +0900

    find solution

--- Week3/17.someRecursive.js
+++ Week3/17.someRecursive.js
@@ -6,11 +6,19 @@
 // someRecursive([4,6,8], isOdd) // false
 // someRecursive([4,6,8], val => val > 10); // false
 
-function someRecursive(a) {
-    // if (a > 10) {
-    //     return false
-    // }
-    // let e = a.split("");
+/**array와 callback함수를 인자로 받고, array의 요소 중 하나라도 
+ * callback 함수의 반환값이 true라면 true를 반환하고 그게 아니라면 false를 반환하는 회귀 함수 구현 */
 
-    return a == (a.split("")) % 2 !== 0
+const isOdd = val => val % 2 !== 0;
+
+/** 솔루션 답안 */
+function someRecursive(array, callback) {
+    if (array.length === 0) return false;
+    if (callback(array[0])) return true;
+    return someRecursive(array.slice(1), callback);
 }
+
+
+/** 좋은 해설 
+ * https://stackoverflow.com/questions/73908045/why-callback-needs-arr0-to-return-true
+*/
