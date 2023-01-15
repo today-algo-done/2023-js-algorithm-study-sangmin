bc9f5fa21288634f2bbedcad6bfcb30fc2ee6901
Citizen <40601526+ViGilanteAF@users.noreply.github.com>
2023-01-15 10:10:21 +0900

    () -> []

--- Week4/19.capitalizefirst.js
+++ Week4/19.capitalizefirst.js
@@ -1,7 +1,7 @@
 function capitalizeFirst(str) {
     // add whatever parameters you deem necessary - good luck!
     return str.map((word) => {
-        const cap = word.charAt(0).toUpperCase();
+        const cap = word.charAt[0].toUpperCase();
         const result = word.slice(1).toLowerCase();
         return cap + result
     })
