36e9e71e59eb16e15928aaf48bb282531b5c0239
Citizen <40601526+ViGilanteAF@users.noreply.github.com>
2023-01-23 01:51:41 +0900

    10773_Zero_javascript_ver

---
+++ Week5/10773_Zero.js
@@ -0,0 +1,20 @@
+function solve(num) {
+    let n = num;
+    const arr = [];
+
+    for (let i; i < n.length; i++) {
+        let a = num.indexOf(0);
+
+        if (a != 0) {
+            arr.append(a)
+        } else {
+            arr.pop()
+        }
+        return arr.reduce(function (accumulator, currentValue) {
+            return accumulator + currentValue
+        })
+    }
+
+}
+
+solve(43040)
