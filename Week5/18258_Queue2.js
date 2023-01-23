118c2da6d24055ff0d04d4ff8c21001a257a67f0
Citizen <40601526+ViGilanteAF@users.noreply.github.com>
2023-01-23 15:16:28 +0900

    Queue2

---
+++ Week5/18258_Queue2.js
@@ -0,0 +1,17 @@
+function que2(arr = []) {
+    for (let i = 0; i < arr.length; i++) {
+        arr = arr.splice(i)
+
+        if (arr[0] == 'push') {
+            arr.append(arr[1])
+        } else if (arr[0] === 'pop') {
+            arr.append(pop()).append())
+        }
+
+    }
+}
+
+
+
+
+que2(15, push, 1, push, 2, front, back, size, empty, pop, pop, pop, size, empty, pop, push, 3, empty, front);
