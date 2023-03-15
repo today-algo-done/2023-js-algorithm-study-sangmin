041a6ee0020791f86253e2be54a51880e14975a1
HU SANG MIN <40601526+ViGilanteAF@users.noreply.github.com>
2023-03-16 03:08:22 +0900

    방향키에 따른 위치 이동 왼쪽으로 이동시 임시저장후 오른쪽으로 이동할때 붙이기(join) 배열 2개 선언 1개는 문자 저장 하면서 현재위치파악 1개는 방향키에 따른 문자 처리
    if 조건문을 이용해서 방향키에 따라 pop할거 설정, pop한문자 는 반대쪽 배열 에 연결
    
    '-' 나오면 그냥 pop
    
    마지막에 join으로 두배열 합치기
    이때 문자를 빼서 저장한 공간 은 먼저 빠진 문자가 오른쪽 끝값으로 와야하기 때문에 reverse 를 해서 붙임

--- Week5/5397_KeyLogger.js
+++ Week5/5397_KeyLogger.js
@@ -1,10 +1,23 @@
-// 2
-// <<BP<A>>Cd-
-// ThIsIsS3Cr3t
+const input = require("fs").readFileSync("/Users/husangmin/Documents/GitHub/stdin").toString().trim().split("\n").map((str) => str.trim())
+const [T, ...Pwd] = input;
 
-// BAPC
-// ThIsIsS3Cr3t
-function keylogger() {
-
+function keylogger(T, Pwd) {
+    Pwd.map((pwd) => {
+        let [answer, tmp] = [[], []];
+        pwd.split("").map((str) => {
+            if (v === "<") {
+                if (answer.length !== 0) tmp.push(answer.pop());
+            } else if (v === ">") {
+                if (tmp.length !== 0) answer.push(tmp.pop());
+            } else if (v === "-") {
+                answer.pop();
+            } else {
+                answer.push(str);
+            }
+        });
+        console.log([...answer, ...tmp.reverse()].join(""));
+    });
 
 }
+
+keylogger(T, Pwd);
