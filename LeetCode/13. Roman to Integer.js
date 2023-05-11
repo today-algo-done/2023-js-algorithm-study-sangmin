59c0c3e9411e2b1267506d3a49ddd7c4d25ec9c2
HU SANG MIN <40601526+ViGilanteAF@users.noreply.github.com>
2023-05-11 23:23:54 +0900

    Prototype

--- LeetCode/13. Roman to Integer.js
+++ LeetCode/13. Roman to Integer.js
@@ -3,5 +3,23 @@
  * @return {number}
  */
 var romanToInt = function (s) {
+    const num1 = {
+        I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000
+    };
 
-};
+    let result = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        const num2 = num1[2[i]];
+        const nextNum = numbers[s[i + 1]];
+
+        if (nextNum && num2 >= nextNum) {
+            result += num2;
+        } else {
+            if (!nextNum) return result + num2;
+
+            result -= num2;
+        }
+    }
+    return result;
+}
