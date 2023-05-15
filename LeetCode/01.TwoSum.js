2f4ec6c9f57dd2e7bf5fdb5306cc03a318e84895
HU SANG MIN <40601526+ViGilanteAF@users.noreply.github.com>
2023-05-16 07:43:18 +0900

    use Double for Loop solve it.

---
+++ LeetCode/01.TwoSum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    let len = nums.length;
+
+    for (let i = 0; i < len - 1; i++) {
+        for (let j = i + 1; j < len; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j]
+            }
+        }
+
+    }
+};
